0ae2edb12e4fbe95a293275d5302e09db30b060f
Yura Buzorin <pourquoi-pas@mail.ru>
2023-06-06 00:26:49 +1000

    Заканчиваю 6 раздел

---
+++ source/js/dist/toggle.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let toggle = document.querySelector('.toggle');
+let popover = document.querySelector('.popover');
+toggle.addEventListener('click', function () {
+  if (toggle.classList.contains('header__toggle--burger')) {
+    toggle.classList.remove('header__toggle--burger');
+    toggle.classList.add('header__toggle--close');
+  } else {
+    toggle.classList.add('header__toggle--burger');
+    toggle.classList.remove('header__toggle--close');
+  }
+});
+toggle.addEventListener('click', function () {
+  if (popover.classList.contains('popover--close')) {
+    popover.classList.remove('popover--close');
+    popover.classList.add('popover--open');
+  } else {
+    popover.classList.add('popover--close');
+    popover.classList.remove('popover--open');
+  }
+});
+//# sourceMappingURL=toggle.dev.js.map
